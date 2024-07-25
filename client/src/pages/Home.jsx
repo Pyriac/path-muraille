72d62ed7f0eddd7eee07c5068bd7337f51f90379
ChachaMm17 <charlotte.morvant@gmail.com>
2024-07-25 15:31:46 +0200

    Add a form to apply + CSS

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { useLoaderData } from "react-router-dom";
 
 import Papa from "papaparse";
 
+import "../assets/styles/application.css";
+
 export default function Home() {
   const jobsFromLoader = useLoaderData();
 
@@ -19,7 +21,42 @@ export default function Home() {
   return (
     <section>
       <h1>coucou from Home</h1>
-      <h2>{data[0].Sexe}</h2>
+      <h2>{data[0].sex}</h2>
+      <section>
+        <form className="form">
+          <div className="flex">
+            <label>
+              <input required="" placeholder="" type="text" className="input" />
+              <span>Prénom</span>
+            </label>
+
+            <label>
+              <input required="" placeholder="" type="text" className="input" />
+              <span>Nom</span>
+            </label>
+          </div>
+          <label>
+            <input required="" placeholder="" type="text" className="input" />
+            <span>email</span>
+          </label>
+          <label>
+            <input required="" type="" placeholder="" className="input" />
+            <span>CV</span>
+          </label>
+          <label>
+            <textarea
+              required=""
+              rows="3"
+              placeholder=""
+              className="input01"
+            ></textarea>
+            <span>Message</span>
+          </label>
+          <button className="buttonOrange" type="submit">
+            Envoyer ma candidature
+          </button>
+        </form>
+      </section>
     </section>
   );
 }
