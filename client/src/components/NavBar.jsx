61307bc69a883aeb72278498067fc3f8455e513a
Your Name <ewan16270409@outlook.fr>
2024-07-25 19:10:47 +0200

    Add links for Home and About us

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,16 @@
 import logo from "../assets/images/path-muraille1.png";
 import "../assets/styles/navbar.css";
+import { Link } from "react-router-dom";
+
 export default function NavBar() {
   return (
     <nav>
-      <img className="Logo" src={logo} alt="logo-path-muraille" />
-      <h2>About Us</h2>
+      <Link to="/">
+        <img className="Logo" src={logo} alt="logo-path-muraille" />
+      </Link>
+      <Link to="/about">
+        <h2>About Us</h2>
+      </Link>
     </nav>
   );
 }
