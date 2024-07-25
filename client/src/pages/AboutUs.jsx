94b6dc71a9d2481ec120a313c0a85e14e6a1bda3
Your Name <ewan16270409@outlook.fr>
2024-07-25 18:44:51 +0200

    Add about_us

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,3 +1,17 @@
+import "../assets/styles/aboutUs.css";
+import China from "../assets/images/About_us_China.svg";
+
 export default function AboutUs() {
-  return <h1>coucou from AboutUs</h1>;
+  return (
+    <div className="about-us-container">
+      <h1 className="NOS-VALEURS">NOS VALEURS</h1>
+      <img src={China} alt="China" className="China_About" />
+      <div className="values-container">
+        <div className="values">
+          <p>Des plages de travail d’un maximum de 48h d’affilée.</p>
+          <p>Une rémunération en repas à hauteur d’un repas par jour.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
