fa42b4b64aa8cd2a8c23d3fd98bb809cd2ee4e2f
Your Name <ewan16270409@outlook.fr>
2024-07-25 15:39:30 +0200

    Add Card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import PropTypes from "prop-types";
 import "./assets/styles/app.css";
+import Coeur from "./assets/Vector.svg";
 
-function App() {
-  const [count, setCount] = useState(0);
+const Card = ({ title, company, location, description }) => {
+  return (
+    <div className="card">
+      <div className="card-header">
+        <h2>{title}</h2>
+      </div>
+
+      <div className="card-body">
+        <p className="company">{company}</p>
+        <p>{description}</p>
+        <p>{location}</p>
+      </div>
+      <div className="card-footer">
+        <button>Voir les détails</button>
+        <img src={Coeur} alt="" />
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  company: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+const App = () => {
+  const jobData = [
+    {
+      title: "CHARPENTIER H/F",
+      company: "WoodWorksChina",
+      location: "Badaling - Pékin",
+      description: "Constructions en bois",
+    },
+    {
+      title: "CHARPENTIER H/F",
+      company: "WoodWorksChina",
+      location: "Badaling - Pékin",
+      description: "Constructions en bois",
+    },
+    {
+      title: "CHARPENTIER H/F",
+      company: "WoodWorksChina",
+      location: "Badaling - Pékin",
+      description: "Constructions en bois",
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+      {jobData.map((job, index) => (
+        <Card
+          key={index}
+          title={job.title}
+          company={job.company}
+          location={job.location}
+          description={job.description}
+        />
+      ))}
+    </div>
   );
-}
+};
 
 export default App;
