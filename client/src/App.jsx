9b1478db4f9fcc2f2a948f3b855695082fdb76bf
ChachaMm17 <charlotte.morvant@gmail.com>
2024-07-26 08:05:52 +0200

    footer creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 import "./assets/styles/app.css";
 
@@ -11,6 +12,7 @@ function App() {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
